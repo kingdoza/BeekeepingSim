3ffb1e8d38ee81da8f327758c0fe4de8b6d9992b
kingdoza <kdowon2k98@naver.com>
2026-07-27 17:57:43 +0900

    5.8 update

--- Source/BeekeepingSimEditor.Target.cs
+++ Source/BeekeepingSimEditor.Target.cs
@@ -8,8 +8,8 @@ public class BeekeepingSimEditorTarget : TargetRules
 	public BeekeepingSimEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("BeekeepingSim");
 	}
 }
